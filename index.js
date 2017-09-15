a9b2510b83da404ebc7b76a0b3da5779994f4b2e
Reidy <x876007@svrn.sovereignbank.com>
2017-09-15 10:50:26 -0400

    user-interface gen

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-var projectNames = document.querySelector('.project-names')
-projectNames.addEventListener('click', function(e){
+var table1 = document.querySelector('#table1')
+table1.addEventListener('click', function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-        var text = target.textContent || text.innerText;
-        element = document.getElementById(e.target.id);
-        if((element.id).match(/proj/g) !== null){
-            if((element.id).match(/week/g) !== null){
-               var textEntered = prompt("You wish to change the employee capacity of this cell. The current value is listed in the input box", text);
+    var text = target.textContent || text.innerText;
+    element = document.getElementById(e.target.id);
+    var intPattern = /[0-9]/g;
+    if ((element.id).match(/cell[0-9]/g) !== null) {
+        var selectedElement = intPattern.exec(element.id).pop();
+        alert("You selected cell " + selectedElement);
+        var textEntered = prompt("What would you like to change this cell to?", text);
+        if (textEntered.match(/[0-9]/g)) {
+            var popChange = textEntered;
+            element.innerHTML = popChange;
+        } else {
+            alert("you Didn't enter a number");
+        }
+    } else {
+        "Please select a table cell containing a number";
+    }
+}, false);
 
+var table2 = document.querySelector('#table2')
+table2.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+    var text = target.textContent || text.innerText;
+    element = document.getElementById(e.target.id);
+    var intPattern = /[0-9]/g;
+    if ((element.id).match(/cell[0-9]/g) !== null) {
+        var selectedElement = intPattern.exec(element.id).pop();
+        alert("You selected cell " + selectedElement);
+        alert("This cell will increment");
+        selectedElement = parseInt(selectedElement, 10) + 1;
+        element.innerHTML = selectedElement;
+    } else {
+        "Please select a table cell containing a number";
+    }
 }, false);
